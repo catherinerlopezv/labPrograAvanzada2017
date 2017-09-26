2305f56d296f0bfbb7f4fa3b8faac38b4560fcca
catherinerlopezv <catherinerlopezv@gmail.com>
2017-09-25 21:04:32 -0600

    Add files via upload

---
+++ console-cpp-lab4Tarea/hello.cc
@@ -0,0 +1,45 @@
+// Simple Hello World
+ 
+#include <iostream>
+ using namespace std;
+ 
+int main(){
+ /*int arreglo[10];
+ 
+ //contadores
+  int i;
+  int j;
+  // ciclos para que se pueda mostrar la tabla 
+    for (i=1;i<11;i++)
+    for (j=1;j<11;j++) {
+        // arrleglo -1 porque es el tamaño del arreglo y se necesita empzar de 1
+        arreglo[i-1]=i*j;
+        }
+   for (i=0;i<10;i++) {
+    cout<<arreglo[i]<<endl;
+    }
+*/
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+ int sqrs[10];
+ cout<<" el arrelo es de 10   "<<endl;
+ int i;
+ 
+ for (i=1;i<11;i++) {
+ sqrs[i-1]=i*i;
+  int suma = suma+i ;
+  cout<<" esta es la suma   "<<suma<<endl;
+ }
+ int sum= 6+10+28+36;
+ int sumaimp=1+3+15+21+45+55;
+ cout<<" esta es la suma de los pares "<<sum<<endl;
+ cout<<" esta es la suma  de los impares "<<sumaimp<<endl;
+
+ return 0;
+}
