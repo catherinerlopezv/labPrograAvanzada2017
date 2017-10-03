22e5676c079588b36c131975b8a39fd17ce1387f
catherinerlopezv <catherinerlopezv@gmail.com>
2017-10-02 20:41:47 -0600

    Add files via upload

---
+++ console-cpp-lab5tarea/hello.cc
@@ -0,0 +1,18 @@
+// Simple Hello World
+ 
+#include <iostream>
+ #include <stdlib.h>
+using namespace std;
+ 
+struct Node{
+        private char propiedad:
+        struct Node next:
+}
+
+int main()
+{
+    
+
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
